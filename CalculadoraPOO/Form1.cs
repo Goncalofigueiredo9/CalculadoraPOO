7b7ee1b2ea9a05ddb31d715537215ecd63ca4434
GoncaloF <GoncaloF@WIN-GESFLHLR4EE>
2019-11-11 17:40:35 +0000

    Adicionar arquivos de projeto.

---
+++ CalculadoraPOO/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraPOO
+{
+    public partial class Form1 : Form
+    {
+        Calculadora casio = new Calculadora();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clicar(object sender, EventArgs e)
+        {
+            casio.input = ((Button)sender).Tag.ToString();
+            label1.Text = casio.output;
+        }
+    }
+}
